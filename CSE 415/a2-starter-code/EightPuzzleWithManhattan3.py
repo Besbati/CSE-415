b134b7bf7918202e6dc7511c633378dc314b1128
RoyL14 <royl14@uw.edu>
2025-10-19 23:18:41 -0700

    Update to manhattan3

--- CSE 415/a2-starter-code/EightPuzzleWithManhattan3.py
+++ CSE 415/a2-starter-code/EightPuzzleWithManhattan3.py
@@ -2,7 +2,7 @@
 This file augments EightPuzzle.py with heuristic information,
 so that it can be used by an A* implementation.
 The particular heuristic is a modification of the Manhattan Distance (sum of horizontal and vertical distances from the expected position)
-The heuristic adds 2 for every pair that needs to swap positions but is in the same row or column.
+The heuristic adds 2 for every pair that needs to swap positions.
 """
 
 from EightPuzzle import *
@@ -18,6 +18,6 @@ def h(s):
                 expected_col = tile % 3
                 manhattan_distance += abs(expected_row - i) + abs(expected_col - j)
                 current_tile = s.b[expected_row][expected_col]
-                if current_tile // 3 == i and current_tile % 3 == j and (expected_row == i ^ expected_col == j):
+                if current_tile // 3 == i and current_tile % 3 == j:
                     manhattan_distance += 1 # this adds 1 for the first object in the pair and adds another for the second object in the pair
     return manhattan_distance
