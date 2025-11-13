95ede7ffc40b7eb34924758f2ebc07a84c9c5708
Lucas Besbati <besbati@uw.edu>
2025-11-12 22:12:19 -0800

    last try zobrist

--- CSE 415/a4-starter-code/besbati_KInARow.py
+++ CSE 415/a4-starter-code/besbati_KInARow.py
@@ -1,50 +1,51 @@
 '''
-<yourUWNetID>_KInARow.py
-Authors: Besbati, Lucas and Lee, Roy
-
+besbati_KInARow.py
+Authors: Besbati, Lucas
 
 An agent for playing "K-in-a-Row with Forbidden Squares" and related games.
 CSE 415, University of Washington
 
-THIS IS A TEMPLATE WITH STUBS FOR THE REQUIRED FUNCTIONS.
-YOU CAN ADD WHATEVER ADDITIONAL FUNCTIONS YOU NEED IN ORDER
-TO PROVIDE A GOOD STRUCTURE FOR YOUR IMPLEMENTATION.
-
+With Zobrist Hashing Implementation
 '''
 
 from agent_base import KAgent
 from game_types import State, Game_Type
 
-AUTHORS = 'Lucas Besbati, Roy Lee' 
-UWNETIDS = ['besbati'] # The first UWNetID here should
-# match the one in the file name, e.g., janiesmith99_KInARow.py.
+AUTHORS = 'Besbati, Lucas; Roy Lee' 
+UWNETIDS = ['besbati, royl14']
 
-import time # You'll probably need this to avoid losing a
-# game due to exceeding a time limit.
+import time
 import game_types
 import math
+import random
 
 INF = math.inf
+
 # Create your own type of agent by subclassing KAgent:
 
-class OurAgent(KAgent):  # Keep the class name "OurAgent" so a game master
-    # knows how to instantiate your agent class.
-
+class OurAgent(KAgent):
     def __init__(self, twin=False):
-        self.twin=twin
+        self.twin = twin
         self.nickname = 'Peter'
         if twin: self.nickname += '2'
         self.long_name = 'Dinklebot'
         if twin: self.long_name += ' II'
         self.persona = 'blunt'
         self.voice_info = {'Chrome': 10, 'Firefox': 2, 'other': 0}
-        self.playing = "X" # e.g., "X" or "O".
+        self.playing = "X"
         self.alpha_beta_cutoffs_this_turn = -1
         self.num_static_evals_this_turn = -1
         self.zobrist_table_num_entries_this_turn = -1
         self.zobrist_table_num_hits_this_turn = -1
         self.current_game_type = None
         self.playing_mode = KAgent.DEMO
+        
+        # Zobrist hashing structures
+        self.zobrist_table = {}  # Hash -> (depth, value, move)
+        self.zobrist_keys = None  # Will be initialized in prepare()
+        self.zobrist_writes = 0
+        self.zobrist_read_attempts = 0
+        self.zobrist_successful_reads = 0
 
     def introduce(self):
         intro = '\nMy name is Peter Dinklage, but you can call me Dinklebot.\n'+\
@@ -54,145 +55,223 @@ class OurAgent(KAgent):  # Keep the class name "OurAgent" so a game master
         if self.twin: intro += "I'm Dinklebot II, no relation.\n"
         return intro
 
-    # Receive and acknowledge information about the game from
-    # the game master:
     def prepare(
         self,
         game_type,
         what_side_to_play,
         opponent_nickname,
-        expected_time_per_move = 0.1, # Time limits can be
-                                      # changed mid-game by the game master.
+        expected_time_per_move=0.1,
+        utterances_matter=True):
+        
+        if utterances_matter:
+            pass
+        
+        self.current_game_type = game_type
+        self.what_side_to_play = what_side_to_play
+        self.time_limit = expected_time_per_move
+        
+        # Initialize Zobrist keys for this game type
+        self._initialize_zobrist_keys(game_type.n, game_type.m)
+        
+        return "OK"
+    
+    def _initialize_zobrist_keys(self, n_rows, m_cols):
+        """Initialize random Zobrist keys for each position and piece type"""
+        random.seed(42)  # Use a fixed seed for consistency
+        self.zobrist_keys = {}
+        
+        # Create random keys for X and O at each position
+        for i in range(n_rows):
+            for j in range(m_cols):
+                self.zobrist_keys[(i, j, 'X')] = random.getrandbits(64)
+                self.zobrist_keys[(i, j, 'O')] = random.getrandbits(64)
+    
+    def _compute_zobrist_hash(self, state):
+        """Compute Zobrist hash for a given state"""
+        hash_value = 0
+        board = state.board
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                piece = board[i][j]
+                if piece in ['X', 'O']:
+                    hash_value ^= self.zobrist_keys[(i, j, piece)]
+        
+        return hash_value
+    
+    def _lookup_zobrist(self, state, depth):
+        """Look up a state in the Zobrist table"""
+        self.zobrist_read_attempts += 1
+        hash_value = self._compute_zobrist_hash(state)
+        
+        if hash_value in self.zobrist_table:
+            stored_depth, stored_value, stored_move = self.zobrist_table[hash_value]
+            # Only use if stored depth is >= current depth (more or equally thorough search)
+            if stored_depth >= depth:
+                self.zobrist_successful_reads += 1
+                return stored_value, stored_move
+        
+        return None, None
+    
+    def _store_zobrist(self, state, depth, value, move):
+        """Store a state evaluation in the Zobrist table"""
+        hash_value = self._compute_zobrist_hash(state)
+        
+        # Only store if we don't have this position or if new search is deeper
+        if hash_value not in self.zobrist_table or self.zobrist_table[hash_value][0] < depth:
+            self.zobrist_table[hash_value] = (depth, value, move)
+            self.zobrist_writes += 1
 
-        utterances_matter=True):      # If False, just return 'OK' for each utterance,
-                                      # or something simple and quick to compute
-                                      # and do not import any LLM or special APIs.
-                                      # During the tournament, this will be False..
-       if utterances_matter:
-           pass
-           # Optionally, import your LLM API here.
-           # Then you can use it to help create utterances.
-           
-       # Write code to save the relevant information in variables
-       # local to this instance of the agent.
-       # Game-type info can be in global variables.
-       self.current_game_type = game_type
-       self.what_side_to_play = what_side_to_play
-       self.time_limit = expected_time_per_move
-
-       # print("Change this to return 'OK' when ready to test the method.")
-       return "OK"
-   
-    # The core of your agent's ability should be implemented here:             
     def make_move(self, current_state, current_remark, time_limit=1000,
-                  use_alpha_beta=True,
-                  use_zobrist_hashing=False, max_ply=3,
-                  special_static_eval_fn=None):
+                    use_alpha_beta=True,
+                    use_zobrist_hashing=False, max_ply=3,
+                    special_static_eval_fn=None):
         print("make_move has been called")
 
-        print("code to compute a good move should go here.")
-
+        # Reset statistics for this turn
         self.alpha_beta_cutoffs_this_turn = 0
         self.num_static_evals_this_turn = 0
+        self.zobrist_writes = 0
+        self.zobrist_read_attempts = 0
+        self.zobrist_successful_reads = 0
+        
+        # Clear Zobrist table if hashing is disabled
+        if not use_zobrist_hashing:
+            self.zobrist_table = {}
 
+        # Use special static eval if in autograder mode
         original_static_eval = None
         if (self.playing_mode == KAgent.AUTOGRADER and special_static_eval_fn is not None):
             original_static_eval = self.static_eval
             self.static_eval = special_static_eval_fn
         
         print("Calling minimax, please defend me while I compute the value.")
-        best_move, val = self.minimax(current_state, max_ply, True)
+        best_move, val = self.minimax(
+            current_state, 
+            max_ply, 
+            use_alpha_beta,
+            use_zobrist_hashing
+        )
         
+        # Restore original static eval
         if original_static_eval is not None:
             self.static_eval = original_static_eval
         
         if not best_move:
             print(current_state)
             print(val)
+        
         new_state = do_move(current_state, best_move[0], best_move[1], other(current_state.whose_move))
-        # Here's a placeholder:
-        # a_default_move = (0, 0)  This might be legal ONCE in a game,
-        # if the square is not forbidden or already occupied.
-    
-        # new_state = current_state # This is not allowed, and even if
-        # it were allowed, the newState should be a deep COPY of the old.
+        
+        new_remark = "I'm done... for now." #commit check
 
-        if self.playing == "X":
-            compare_function = lambda a, b: a >= b
-        else:
-            compare_function = lambda a, b: a <= -b
-
-        if compare_function(val, INF):
-            new_remark = "Victory is mine!"
-        elif compare_function(val, -INF):
-            new_remark = "NO! I CANNOT LOSE!!"
-        if val == 0:
-            new_remark = "Evenly matched... for now."
-        elif compare_function(val, 0):
-            new_remark = "I will seize the advantage!"
-        else:
-            new_remark = "This agent is doing quite well... but not for long."
+        # Update zobrist statistics
+        self.zobrist_table_num_entries_this_turn = len(self.zobrist_table)
+        self.zobrist_table_num_hits_this_turn = self.zobrist_successful_reads
 
         if self.playing_mode == KAgent.AUTOGRADER:
-            stats = [self.alpha_beta_cutoffs_this_turn, self.num_static_evals_this_turn, self.zobrist_table_num_entries_this_turn, self.zobrist_table_num_hits_this_turn]
+            stats = [
+                self.alpha_beta_cutoffs_this_turn, 
+                self.num_static_evals_this_turn, 
+                self.zobrist_table_num_entries_this_turn, 
+                self.zobrist_table_num_hits_this_turn
+            ]
             return [[best_move, new_state] + stats, new_remark]
         else:
             return [[best_move, new_state], new_remark]
 
-    # The main adversarial search function:
     def minimax(self,
             state,
             depth_remaining,
             pruning=False,
+            use_zobrist=False,
             alpha=-INF,
             beta=INF):
         
-        child_states = move_gen(state)
+        # Check Zobrist table first if enabled
+        if use_zobrist:
+            cached_value, cached_move = self._lookup_zobrist(state, depth_remaining)
+            if cached_value is not None:
+                return cached_move, cached_value
+        
+        # Generate child states
+        child_states = list(move_gen(state))
+        
+        # Evaluate current state
         self.num_static_evals_this_turn += 1
         current_eval = self.static_eval(state)
 
+        # Base cases: depth 0 or terminal state
         if depth_remaining == 0 or not child_states or current_eval == INF or current_eval == -INF:
             return None, current_eval
 
+        # Set up for maximizing or minimizing player
         if state.whose_move == "X":
             compare_function = lambda a, b: a >= b
             best_val = -INF
-        else: # "O" player
+        else:  # "O" player
             compare_function = lambda a, b: a <= b
             best_val = INF
+        
         best_move = None
+        
+        # Optional: Order children by static evaluation for better pruning
+        if use_zobrist:
+            # Check if children have cached values for ordering
+            child_evals = []
+            for move, child_state in child_states:
+                cached_val, _ = self._lookup_zobrist(child_state, 0)
+                if cached_val is not None:
+                    child_evals.append((move, child_state, cached_val))
+                else:
+                    child_evals.append((move, child_state, self.static_eval(child_state)))
+            
+            # Sort based on whether we're maximizing or minimizing
+            if state.whose_move == "X":
+                child_evals.sort(key=lambda x: x[2], reverse=True)
+            else:
+                child_evals.sort(key=lambda x: x[2])
+            
+            child_states = [(move, child_state) for move, child_state, _ in child_evals]
+        
+        # Search through children
         for move, child_state in child_states:
-            result = self.minimax(child_state, depth_remaining - 1, pruning = True, alpha = alpha, beta = beta)
+            result = self.minimax(
+                child_state, 
+                depth_remaining - 1, 
+                pruning=pruning,
+                use_zobrist=use_zobrist,
+                alpha=alpha, 
+                beta=beta
+            )
             child_val = result[1]
 
             if compare_function(child_val, best_val):
                 if pruning:
                     if state.whose_move == "X":
-                        alpha = max(alpha, best_val)
+                        alpha = max(alpha, child_val)
                     else:
-                        beta = min(beta, best_val)
+                        beta = min(beta, child_val)
+                    
                     if alpha >= beta:
                         self.alpha_beta_cutoffs_this_turn += 1
-                        return best_move, child_val
+                        # Store in Zobrist table before returning
+                        if use_zobrist:
+                            self._store_zobrist(state, depth_remaining, child_val, move)
+                        return move, child_val
+                
                 best_move = move
                 best_val = child_val
         
+        # Store result in Zobrist table
+        if use_zobrist:
+            self._store_zobrist(state, depth_remaining, best_val, best_move)
+        
         return best_move, best_val
-
-
-        # default_score = 0 # Value of the passed-in state. Needs to be computed.
-
-    
-        # return [default_score, "my own optional stuff", "more of my stuff"]
-        # Only the score is required here but other stuff can be returned
-        # in the list, after the score, in case you want to pass info
-        # back from recursive calls that might be used in your utterances,
-        # etc. 
-    
 
     def static_eval(self, state, game_type=None):
         board = state.board
-        if game_type ==None:
+        if game_type == None:
             game_type = self.current_game_type
         k = game_type.k
         mCols = game_type.m
@@ -200,32 +279,23 @@ class OurAgent(KAgent):  # Keep the class name "OurAgent" so a game master
 
         eval_result = 0
         for i in range(nRows):
-            eval_result += self.check_section(board, k, i, 0, 0, 1) # row
+            eval_result += self.check_section(board, k, i, 0, 0, 1)  # row
             if eval_result == INF or eval_result == -INF:
                 return eval_result
-            eval_result += self.check_section(board, k, i, 0, 1, 1) # diag right
-            if i > 0:
-                if eval_result == INF or eval_result == -INF:
-                    return eval_result
-                eval_result += self.check_section(board, k, i, mCols - 1, 1, -1) # diag left
-                if eval_result == INF or eval_result == -INF:
-                    return eval_result
         for j in range(mCols):
-            eval_result += self.check_section(board, k, 0, j, 1, 0) # column
+            eval_result += self.check_section(board, k, 0, j, 1, 0)  # column
             if eval_result == INF or eval_result == -INF:
                 return eval_result
-            eval_result += self.check_section(board, k, 0, j, 1, 1) # diag right
+            eval_result += self.check_section(board, k, 0, j, 1, 1)  # diag right
             if eval_result == INF or eval_result == -INF:
                 return eval_result
-            eval_result += self.check_section(board, k, 0, j, 1, -1) # diag left
+            eval_result += self.check_section(board, k, 0, j, 1, -1)  # diag left
             if eval_result == INF or eval_result == -INF:
                 return eval_result
-        #print(state)
-        #print(eval_result)
 
         return eval_result
     
-    def check_section(self, board, k, row, col, rowStep, colStep): # rowStep can be 1, 0 or -1, colstep 1 or 0
+    def check_section(self, board, k, row, col, rowStep, colStep):
         if rowStep * k + row > len(board):
             return 0
         if colStep * k + col > len(board[0]):
@@ -241,15 +311,11 @@ class OurAgent(KAgent):  # Keep the class name "OurAgent" so a game master
         while (rowStep * i + row < len(board) and colStep * i + col < len(board[0]) and colStep * i + col >= 0):
             curr = board[rowStep * i + row][colStep * i + col]
             if curr == ' ':
-                #if mainStreak > 0: # X - < right side
                 rightSpaceStreak += 1
-                #else: # - X < left side
-                    #leftSpaceStreak += 1
-
             elif curr == 'X' or curr == 'O':
-                if curr != streakItem: # - - - X - O
+                if curr != streakItem:
                     # conclude the last streak
-                    eval += self.addScore(k, rightSpaceStreak, leftSpaceStreak, mainStreak,streakItem)
+                    eval += self.addScore(k, rightSpaceStreak, leftSpaceStreak, mainStreak, streakItem)
                     # switch to new streak
                     streakItem = curr
                     mainStreak = 1
@@ -262,10 +328,9 @@ class OurAgent(KAgent):  # Keep the class name "OurAgent" so a game master
                             return INF
                         if streakItem == 'O':
                             return -INF
-            
             else:
                 # conclude the last streak
-                eval += self.addScore(k, rightSpaceStreak, leftSpaceStreak, mainStreak,streakItem)
+                eval += self.addScore(k, rightSpaceStreak, leftSpaceStreak, mainStreak, streakItem)
                 # reset
                 streakItem = ''
                 mainStreak = 0
@@ -274,29 +339,27 @@ class OurAgent(KAgent):  # Keep the class name "OurAgent" so a game master
             i += 1
         
         # conclude the final streak if there is one
-        eval += self.addScore(k, rightSpaceStreak, leftSpaceStreak, mainStreak,streakItem)
+        eval += self.addScore(k, rightSpaceStreak, leftSpaceStreak, mainStreak, streakItem)
         
         return eval
     
-    def addScore(self, k, rightSpaceStreak, leftSpaceStreak, mainStreak,streakItem):
+    def addScore(self, k, rightSpaceStreak, leftSpaceStreak, mainStreak, streakItem):
         if mainStreak == 0:
             return 0
         addAmount = 0
-        if rightSpaceStreak + leftSpaceStreak + mainStreak >= k: # guarantee possible placement
+        if rightSpaceStreak + leftSpaceStreak + mainStreak >= k:  # guarantee possible placement
             addAmount += pow(10, mainStreak)
-            if rightSpaceStreak > 0 and leftSpaceStreak > 0: # bonus points for both sides availability
+            if rightSpaceStreak > 0 and leftSpaceStreak > 0:  # bonus points for both sides availability
                 addAmount *= 2
         if streakItem == 'X':
             return addAmount
         else:
             return -addAmount
-        
 
 
- 
-# OPTIONAL THINGS TO KEEP TRACK OF:
+# Helper functions
 def other(p):
-    if p =='X':
+    if p == 'X':
         return 'O'
     return 'X'
 
@@ -319,10 +382,3 @@ def do_move(state, i, j, o):
     news.board[i][j] = state.whose_move
     news.whose_move = o
     return news
-
-#  WHO_MY_OPPONENT_PLAYS = other(WHO_I_PLAY)
-#  MY_PAST_UTTERANCES = []
-#  OPPONENT_PAST_UTTERANCES = []
-#  UTTERANCE_COUNT = 0
-#  REPEAT_COUNT = 0 or a table of these if you are reusing different utterances
-
